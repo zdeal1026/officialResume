7b22d61090519c8bad8f98e130da65aa558531d0
Zdeal <jedimastershake@gmail.com>
2021-10-13 21:47:59 -0400

    components created

---
+++ portfolio/src/components/Bio.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Bio() {
+  return (
+    <div>
+      Hello! My name is Zachary Deal and I just recently graduated from the
+      coding bootcamp at UNC Charlotte!
+    </div>
+  );
+}
+
+export default Bio;
---
+++ portfolio/src/components/Education.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Education() {
+  return <div>education here</div>;
+}
+
+export default Education;
